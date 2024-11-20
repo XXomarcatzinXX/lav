bf894c4a1767b248d8edfe8a6f9966ab55d4cfa9
Josue omar catzin chacon <20393135@utcancun.edu.mx>
2024-11-20 09:44:26 -0500

    se instalo, para que se use con talwqind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 import { PrimeNGConfig } from 'primeng/api';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IconFieldModule, InputIconModule, InputTextModule],
+  imports: [RouterOutlet, IconFieldModule, InputIconModule, InputTextModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit{
   constructor(private primengConfig: PrimeNGConfig){}
 
   ngOnInit(): void {
-      this.primengConfig.ripple = true
+      this.primengConfig.ripple = false
   }
   title = 'lav';
 }
