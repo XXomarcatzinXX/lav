45d84bf28d65234a34304a1f4044a6ff05b34f17
Josue omar catzin chacon <20393135@utcancun.edu.mx>
2024-11-20 09:15:16 -0500

    se hco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { PrimeNGConfig } from 'primeng/api';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IconFieldModule, InputIconModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private primengConfig: PrimeNGConfig){}
+
+  ngOnInit(): void {
+      this.primengConfig.ripple = true
+  }
   title = 'lav';
 }
